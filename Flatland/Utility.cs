5d741cf6d67bc85ee2a9edca3cb7b045f34491d8
Øyvind <oyvindk_93@hotmail.com>
2016-03-01 17:15:10 +0100

    Utility quickfix add

---
+++ Flatland/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flatland
+{
+    public static class Utility
+    {
+        public static int Mod(int x, int m)
+        {
+            return (x % m + m) % m;
+        }
+
+
+    }
+}
