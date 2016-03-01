52cce4fef9e3042363e00709d594a16e98e9d8e3
Øyvind <oyvindk_93@hotmail.com>
2016-03-01 15:48:19 +0100

    Add Evolutionary Algorithm + GUI from Project 2

---
+++ Flatland/EA/AdultSelector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flatland.EA
+{
+    abstract class AdultSelector
+    {
+
+        public int AdultCount { get; set; }
+        abstract public void Select(List<Individual> childPopulation, List<Individual> adultPopulation);
+    }
+}
--- Flatland/EA/FitnessEvaluator.cs
+++ Flatland/EA/FitnessEvaluator.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Flatland
+namespace Flatland.EA
 {
-    class BinaryToANNWeightsDeveloper
+    abstract class FitnessEvaluator
     {
-
-
-
+        abstract public float Evaluate(Individual individual);
     }
 }
