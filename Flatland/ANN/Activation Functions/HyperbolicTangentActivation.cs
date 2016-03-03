00aa375d81aaa215de78f5652b6c60785c033b36
Johannes Moskvil <johannesmos@hotmail.com>
2016-03-03 13:40:18 +0100

    Learn to build before push

--- Flatland/ANN/Activation Functions/HyperbolicTangentActivation.cs
+++ Flatland/ANN/Activation Functions/HyperbolicTangentActivation.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Flatland.ANN
+namespace Flatland
 {
     class HyperbolicTangentActivation : ActivationFunction
     {
